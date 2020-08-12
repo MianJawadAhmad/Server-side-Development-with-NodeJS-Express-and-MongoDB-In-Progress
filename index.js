9a302719b642a3d2ab4655115ee770450aa69217
MianJawadAhmad <a.jawad321@gmail.com>
2020-08-12 17:48:32 +0500

    Simple node example

---
+++ index.js
@@ -0,0 +1,18 @@
+var rect ={
+    parimeter: (x,y) => (2*(x+y)),
+    area: (x,y) => (x*y)
+};
+
+function solveRect(l,b){
+    console.log('solving for rectingal where l = '+l+'and b = '+b);
+    if(l <= 0 || b <= 0){
+        console.log("Rectangle dimensions should be grater than zero: l = "+ l +", and b = "+b);
+    }else{
+        console.log('The area of the rectangle is '+ rect.area(l,b));
+        console.log('The Parimeter of the rectangle is'+ rect.parimeter(l,b));
+    }
+}
+
+solveRect(2,4);
+solveRect(0,5);
+solveRect(-3,5);
